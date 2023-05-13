4b71ab97bc776718f270d59a3e13322e06d2bfd6
倪亞甄 <niyazhen@niyazhendeMacBook-Pro.local>
2023-05-13 21:03:35 +0800

    docs(custom): Update content

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/sashabaranov/go-openai"
-	gpt3 "github.com/sashabaranov/go-openai"
 )
 
 var bot *linebot.Client
@@ -30,6 +29,8 @@ func main() {
 	http.ListenAndServe(addr, nil)
 }
 
+// callbackHandle - 處理LineBot接收到的訊息，並將訊息傳遞給 Open API進行處理
+// 最後將處理結果發送回Line Bot用戶。
 func callbackHandle(bot *linebot.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		// 解析 Line Bot 訊息
@@ -45,15 +46,16 @@ func callbackHandle(bot *linebot.Client) http.HandlerFunc {
 
 		// 處理 Line Bot 訊息
 		for _, event := range events {
+			// 如果接到的訊息類型為「message」，才做訊息處裡
 			if event.Type == linebot.EventTypeMessage {
-
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
+					// 呼叫 getOpenAIRes 函數，根據接收到的Line Bot訊息返回對應的OpenAI 處理結果
 					res, err := getOpenAIRes(message.Text)
 					if err != nil {
 						log.Fatal(err)
 					}
-
+					// 如果處理的結果不為空，則回覆給用戶
 					if res != "" {
 						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(res)).Do(); err != nil {
 							log.Print(err)
@@ -65,9 +67,13 @@ func callbackHandle(bot *linebot.Client) http.HandlerFunc {
 		}
 	}
 }
+
+// newLineBot - 初始化LineBot
 func newLineBot() (*linebot.Client, error) {
 	bot, err := linebot.New(
+		// LineBot的Channel Secret
 		os.Getenv("CHANNEL_SECRET"),
+		// LineBot的Channel Access Token
 		os.Getenv("CHANNEL_ACCESS_TOKEN"),
 	)
 
@@ -79,13 +85,15 @@ func newLineBot() (*linebot.Client, error) {
 
 }
 
+// getOpenAIRes - 取得OpenAI的回覆
 func getOpenAIRes(prompt string) (string, error) {
 	// 初始化 OpenAI API客戶端
-	client := gpt3.NewClient(os.Getenv("OPENAI_API_KEY"))
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 
 	// 設定 Completoms API 參數
-	req := gpt3.ChatCompletionRequest{
-		Model: gpt3.GPT3Dot5Turbo,
+	req := openai.ChatCompletionRequest{
+		// 模型為3.5
+		Model: openai.GPT3Dot5Turbo,
 		// 最大輸出內容
 		MaxTokens: 300,
 		// 輸入的文字
